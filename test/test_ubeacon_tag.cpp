0db5cf7cfb532ada3399018222813444a1ac980e
samuelyhsu <samuel.hsu@qq.com>
2025-04-25 15:13:03 +0800

    - 修复test

--- test/test_ubeacon_tag.cpp
+++ test/test_ubeacon_tag.cpp
@@ -8,6 +8,6 @@ TEST_CASE("checksum") {
   uint8_t data[TOTAL_SIZE] = {0x01, 0x12, 0x23, 0x34, 0x45, 0x56, 0x67, 0x78,
                               0x89, 0x9A, 0xAB, 0xBC, 0xCD, 0xDE, 0xEF};
   REQUIRE(ubeacon_get_sum(data, DATA_SIZE) == sum);
-  update_sum(data, TOTAL_SIZE);
-  REQUIRE(verify_sum(data, TOTAL_SIZE));
+  ubeacon_update_sum(data, TOTAL_SIZE);
+  REQUIRE(ubeacon_verify_sum(data, TOTAL_SIZE));
 }
